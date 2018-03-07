742582a432651a8be87b03f2343fd5d11e71655d
mollynordh <molly@kicksort.se>
2018-03-07 13:01:54 +0100

    Moved calendar icon

--- src/components/YearInput.jsx
+++ src/components/YearInput.jsx
@@ -8,24 +8,24 @@ function YearInput({ value, openPanel, selected, clear, placeholder }) {
   }
 
   return (
-    <div className={`input-wrapper ${selectedClass}`}>
-      <input
-        className="year-input"
-        value={value}
-        onClick={openPanel}
-        placeholder={placeholder}
-        readOnly
-      />
-      <i
-        name="times"
-        className="input-icon input-icon-calendar fa fa-calendar"
-      />
-      <i
-        name="calendar"
-        className="input-icon input-icon-close fa fa-times"
-        onClick={clearHandler}
-      />
-</div>
+  <div className={`input-wrapper ${selectedClass}`}>
+    <i
+      name="times"
+      className="input-icon input-icon-calendar fa fa-calendar"
+    />
+    <i
+      name="calendar"
+      className="input-icon input-icon-close fa fa-times"
+      onClick={clearHandler}
+    />
+    <input
+      className="year-input"
+      value={value}
+      onClick={openPanel}
+      placeholder={placeholder}
+      readOnly
+    />
+  </div>
   );
 }
 
